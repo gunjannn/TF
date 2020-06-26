ec87de8fe940ce7a89c9e712088f45196411aafa
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-06-26 10:48:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,10 @@
-resource "aws_instance" "linux_vm" {
-    ami                         = "${var.aws_ami}"
-    availability_zone           = "eu-central-1a"
-    ebs_optimized               = false
-    instance_type               = "t2.micro"
-    monitoring                  = false
-    key_name                    = "${var.aws_key}"
-    subnet_id                   = "${var.aws_subnet}"
-    source_dest_check = false
-    vpc_security_group_ids      = "sg-540a0f78"
-    associate_public_ip_address = true
-  /*user_data = "${file("user-data.sh")}"*/
-    root_block_device {
-        volume_type           = "gp2"
-        volume_size           = 8
-        delete_on_termination = false
-    }
-    tags = {
-        Name = "linux_vm"
-    }
+provider "aws" {
+  access_key = "AKIA46G6QQAPZW6FYT5A"
+  secret_key = "mH1dvbvHEteEZpw9jwwpqNpgjPF9Nx2t1hNgT/eh"
+  region     = "us-east-1"
 }
 
-
+resource "aws_instance" "example" {
+  ami           = "ami-09d95fab7fff3776c"
+  instance_type = "t2.micro"
+}
