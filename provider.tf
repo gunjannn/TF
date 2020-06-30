22701c18f5ea65ccf634e5d065d231ad34d0e25d
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-06-30 14:03:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,8 @@
-# Provide AWS Access key and Secret Key
-
-provider "aws" {
-  access_key = var.option_1_aws_access_key
-  secret_key = var.option_2_aws_secret_key
-  region     = var.region
+provider "azurerm" {
+  version = "=1.5.0"
 }
 
-# This allows terraform to backup the *.tfstate file to AWS s3 bucket. Uncomment or Remove the lines to disable remote backup and use local state (Not Recommended)
-# terraform {
-#   backend "s3" {
-#     bucket = "mybucket"
-#     key    = "path/to/my/key/some.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-# Here it is initialized with empty parameters. Other params can be passed at "terraform init --backend-config="bucket=mybucket" --backend-config="key=path/to/my/key/some.tfstate" --backend-config="region=us-east-1""
-
-terraform {
-  backend "s3" {
-  }
-}
+export ARM_SUBSCRIPTION_ID=64b70538-bc40-4492-9c4b-13f8b43e732d
+export ARM_CLIENT_ID=3461446c-1154-4720-95f3-6c1309af3507
+export ARM_CLIENT_SECRET=gi5vP2T-G6_UWN-U7rTHG4wCGPwj82vn.d
+export ARM_TENANT_ID=2b6d0a61-dfad-4b8b-8763-0cb831697a57
