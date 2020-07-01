4c06d667da9b7dbaa96e608fa355dc6060b8dfdd
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-01 13:01:00 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,10 @@
 # Variables for accepting Access Key and Secret key for AWS
 # Default region is set to us-east-1
 
+variable "client_id" {
+  description = "c6fd5c6c-5483-4628-95c0-3c1bea294f7a"
+  }
+
 variable "tenant_id" {
   description = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
   }
@@ -9,9 +13,7 @@ variable "subscription_id" {
   description = "64b70538-bc40-4492-9c4b-13f8b43e732d"
   }
 
-variable "client_id" {
-  description = "c6fd5c6c-5483-4628-95c0-3c1bea294f7a"
-  }
+
 
 variable "client_secret" {
   description = "jENlDLFG-8-.5jH1~-Ar9l~752PyQYr-7K"
