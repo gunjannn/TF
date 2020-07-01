918b8b13ccacbaab332401889e827645de6f675d
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-01 12:11:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ service_principal {
   client_id     = "${var.client_id}"
   client_secret = "${var.client_secret}"
   tenant_id = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
   
   }
 
