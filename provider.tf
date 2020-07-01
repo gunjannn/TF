6c26d39b1e6b42fb9b59ddcddabe8d756f7fd204
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-01 11:34:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ features{}
 }
 
 export ARM_SUBSCRIPTION_ID = var.subscription_id
-export ARM_CLIENT_ID = "3461446c-1154-4720-95f3-6c1309af3507"
-export ARM_CLIENT_SECRET = "gi5vP2T-G6_UWN-U7rTHG4wCGPwj82vn.d"
-export ARM_TENANT_ID = "2b6d0a61-dfad-4b8b-8763-0cb831697a57"
+export ARM_CLIENT_ID = var.client_id
+export ARM_CLIENT_SECRET = var.client_secret
+export ARM_TENANT_ID = var.tenant_id
 
