2cfb2b7cd165844f44f31e48d8b20c023cb544cb
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-01 11:42:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
   version = "=2.0.0"
-features{}
 }
-
 export ARM_SUBSCRIPTION_ID = var.subscription_id
 export ARM_CLIENT_ID = var.client_id
 export ARM_CLIENT_SECRET = var.client_secret
