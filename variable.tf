040f15d89f86e8db308523c3c0c30a701dba2048
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-01 12:20:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,16 +6,16 @@ variable "tenant_id" {
   }
 
 variable "subscription_id" {
-  subscription_id = "64b70538-bc40-4492-9c4b-13f8b43e732d"
+  description = "64b70538-bc40-4492-9c4b-13f8b43e732d"
   
   }
 
 variable "client_id" {
-  client_id = "3461446c-1154-4720-95f3-6c1309af3507"
+  description = "3461446c-1154-4720-95f3-6c1309af3507"
 }
 
 variable "client_secret" {
-  client_secret = "gi5vP2T-G6_UWN-U7rTHG4wCGPwj82vn.d"
+  description = "gi5vP2T-G6_UWN-U7rTHG4wCGPwj82vn.d"
 }
 
 
